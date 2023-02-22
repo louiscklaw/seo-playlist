001e8b527c272a2d72eb3a638fd2e3cdacb84bbd
louiscklaw <louiscklaw@users.noreply.github.com>
2023-02-22 17:28:45 +0800

    update docker-next12-seo-tryout,

--- docker-next12-seo-tryout/pages/helloworld/test.jsx
+++ docker-next12-seo-tryout/pages/helloworld/test.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>helloworld test title ?</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="canonical" href="https://docker-next12-seo.iamon99.com/helloworld/test" />
       </Head>
